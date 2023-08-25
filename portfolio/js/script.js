810b95705efc06eb1a8864789cb0f2e5f83fa53c
Zahra Moosavi <mousavi.zahrasadat.1998@gmail.com>
2023-08-25 16:06:57 +0330

    add portfolio ptoject

---
+++ portfolio/js/script.js
@@ -0,0 +1,59 @@
+//toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+menuIcon.onclick=() =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+let sections = [...document.querySelectorAll('section')];
+let navLinks = document.querySelectorAll('header nav a')
+
+// scroll section
+window.onscroll=()=>{
+
+    sections.map(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+
+        if(top > offset && top < offset + height){
+            //active navbar links
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id+ ']').classList.add('active');
+
+            })
+
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+
+        }
+        else{
+
+            sec.classList.remove('show-animate');
+
+        }
+
+        //if want to use animation that repeats on scroll use this
+    })
+
+
+
+
+
+    //steaky headder
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky' , window.scrollY > 100);
+
+    //remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');//* 
+    navbar.classList.remove('active');
+
+}
